9341e1a6be694b85173bf298314730cfc0882ee8
Jan Philip <jan.philip.wahle@gmail.com>
2021-10-26 15:02:03 +0200

    Add template code

---
+++ tests/test_corpus_loader.py
@@ -0,0 +1,19 @@
+import pytest
+from src.data.corpus import DataLoader, DummyLoader
+
+
+def test_corpus_loader() -> None:
+
+    base_loader = DataLoader()
+    corpus = DummyLoader()
+
+    with pytest.raises(NotImplementedError):
+        base_loader.load_data()
+
+    assert len(corpus.get_data()) == 3
+
+    tokenized_documents = DataLoader.tokenize(corpus.get_data(), lower_case=True)
+    for tokenized_document in tokenized_documents:
+        assert len(tokenized_document) == 6
+        for token in tokenized_document:
+            assert token.islower()
