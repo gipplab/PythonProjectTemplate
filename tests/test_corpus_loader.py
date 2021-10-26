11e6e1d85de75cab817b033ead6fc22a8179b4c2
Jan Philip <jan.philip.wahle@gmail.com>
2021-10-26 17:15:43 +0200

    Small test patch #patch

--- tests/test_corpus_loader.py
+++ tests/test_corpus_loader.py
@@ -10,6 +10,7 @@ def test_corpus_loader() -> None:
     with pytest.raises(NotImplementedError):
         base_loader.load_data()
 
+    assert corpus is not None
     assert len(corpus.get_data()) == 3
 
     tokenized_documents = DataLoader.tokenize(corpus.get_data(), lower_case=True)
